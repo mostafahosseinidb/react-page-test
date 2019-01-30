51c9d7ba36a815b24692a014e60d712391c8278c
mostafa hosseini <mostafa.hosseini.db@gmail.com>
2019-01-30 11:13:17 +0330

    Updates

---
+++ precache-manifest.deea3f0e69950e2db025cdaa740b6d8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "83872a88b5e3a538419b",
+    "url": "/react-page-test/static/js/runtime~main.83872a88.js"
+  },
+  {
+    "revision": "fb982035f80783ad7f64",
+    "url": "/react-page-test/static/js/main.fb982035.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-page-test/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "fb982035f80783ad7f64",
+    "url": "/react-page-test/static/css/main.9a4ac9a9.chunk.css"
+  },
+  {
+    "revision": "e31fcdcfaa3dfead13968db089c5cfd9",
+    "url": "/react-page-test/index.html"
+  }
+];
